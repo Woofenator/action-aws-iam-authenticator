f54e6af4d654da0f7d2ed046c569db3ce63473cf
Maris Jirgens <maris.jirgens@cisha.de>
2020-11-17 11:07:05 +0200

    fix: Version variable usage

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,52 @@
-import * as os from 'os';
-import * as util from 'util';
-import * as fs from 'fs';
-
-import * as toolCache from '@actions/tool-cache';
-import * as core from '@actions/core';
+import * as core from "@actions/core";
+import * as toolCache from "@actions/tool-cache";
+import * as fs from "fs";
+import * as os from "os";
+import * as util from "util";
 
 function downloadUrl(version: string): string {
-    switch (os.type()) {
-        case 'Linux':
-            return util.format('https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.4.0/aws-iam-authenticator_%s_linux_amd64', version);
+  switch (os.type()) {
+    case "Linux":
+      return util.format(
+        "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v%s/aws-iam-authenticator_%s_linux_amd64",
+        version,
+        version
+      );
 
-        case 'Darwin':
-            return util.format('https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.4.0/aws-iam-authenticator_%s_darwin_amd64', version);
+    case "Darwin":
+      return util.format(
+        "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v%s/aws-iam-authenticator_%s_darwin_amd64",
+        version,
+        version
+      );
 
-        case 'Windows_NT':
-        default:
-            return util.format('https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.4.0/aws-iam-authenticator_%s_windows_amd64.exe', version);
-
-    }
+    case "Windows_NT":
+    default:
+      return util.format(
+        "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v%s/aws-iam-authenticator_%s_windows_amd64.exe",
+        version,
+        version
+      );
+  }
 }
 
-const binaryName = 'aws-iam-authenticator'
+const binaryName = "aws-iam-authenticator";
 
 async function run() {
-    let version = core.getInput('version', { 'required': true });
-    let cachedToolpath = toolCache.find(binaryName, version);
-    if (!cachedToolpath) {
-        const downloadPath = await toolCache.downloadTool(downloadUrl(version));
-        fs.chmodSync(downloadPath, '0755');
-        cachedToolpath = await toolCache.cacheFile(downloadPath, binaryName, binaryName, version)
-    }
+  let version = core.getInput("version", { required: true });
+  let cachedToolpath = toolCache.find(binaryName, version);
+  if (!cachedToolpath) {
+    const downloadPath = await toolCache.downloadTool(downloadUrl(version));
+    fs.chmodSync(downloadPath, "0755");
+    cachedToolpath = await toolCache.cacheFile(
+      downloadPath,
+      binaryName,
+      binaryName,
+      version
+    );
+  }
 
-    core.addPath(cachedToolpath)
+  core.addPath(cachedToolpath);
 }
 
 run().catch(core.setFailed);
